7268ae1339b6004a42b8f2199e8e726f4f8b6be2
culp3r <mmorales@evolveacadeny.io>
2017-06-30 11:46:18 -0500

    modified print messages

--- fruitBasket.py
+++ fruitBasket.py
@@ -9,10 +9,10 @@ def guess_fruit(basket):
     no_match = 0
     for fruit in range(len(basket)):
         if user_guess == basket[fruit]:
-            print('Correct! {} is in the basket.'.format(user_guess)) 
+            print('Correct! {} is in the fruit basket.'.format(user_guess)) 
         else:
             no_match += 1
     if no_match == len(basket):
-        print('Terrible. {} is not in the basket: {}'.format(user_guess, basket))
+        print('Terrible. {} is not in the fruit basket: {}'.format(user_guess, basket))
 
 main()
